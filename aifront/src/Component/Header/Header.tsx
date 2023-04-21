8078d668b98053abfac7faef68af4416f64a1b99
paul950104@gmail.com <paul950104@gmail.com>
2023-04-21 17:30:16 +0900

    feat: 일기 작성 1차 구성 완료

--- aifront/src/Component/Header/Header.tsx
+++ aifront/src/Component/Header/Header.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Router } from "react-router-dom";
 
 import { HeaderSC } from "./HeaderSC";
 
 const Header : React.FC = () => {
     return(
         <HeaderSC>
-            <NavLink to = '/'>Home</NavLink>
-            <NavLink to = '/JournalPost'>일기쓰기</NavLink>
+                <NavLink to = '/'>홈</NavLink>
+                <NavLink to = '/JournalPost'>일기쓰기</NavLink>
         </HeaderSC>
     )
 }
--- aifront/src/Page/Journal/JournalPost.tsx
+++ aifront/src/Page/Journal/JournalPost.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import axios from "axios";
+import { useRecoilState } from "recoil";
+import { journalState } from "../../Recoil/atoms";
+import { JournalPostMain, JournalPostSC, ButtonDiv } from "./JournalPostSC";
 
 const JournalPost : React.FC = () => {
+    const [journal, setJournal] = useRecoilState(journalState);
 
+    const handleSubmit = async (e : React.MouseEvent) => {
+        e.preventDefault();
+
+        try {
+            await axios.post('URL', {journal})
+            alert('작성이 완료 되었습니다!')
+        } catch(err) {
+            console.log(err)
+            alert('다시 작성해 주세요.')
+        }
+
+    }
     return(
-        <div>
-            Journal
-        </div>
+        <JournalPostMain>
+            <h1>EEUM : 하루 일기</h1>
+            <JournalPostSC>
+                <input 
+                type = 'text'
+                placeholder = "일기를 적어주세요"
+                onChange={(e : React.ChangeEvent<HTMLInputElement>) => {
+                    setJournal(e.target.value)
+                }} />
+            </JournalPostSC>
+            <ButtonDiv>
+                <button onClick = {handleSubmit}>일기 작성</button>
+            </ButtonDiv>
+        </JournalPostMain>
     )
 }
 
