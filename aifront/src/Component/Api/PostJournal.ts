bc0ad0e185a35fa5fe39f03f2e90b3465e685f5f
ashnamuh <ashnamuh@gmail.com>
2023-04-28 19:14:43 +0900

    code review

--- aifront/src/Component/Api/PostJournal.ts
+++ aifront/src/Component/Api/PostJournal.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { JournalRequest, JournalResponse } from "../../Types/journal.type";
 
 export const createJournal = async (body: JournalRequest) => {
-    const res = await axios.post<JournalRequest>('http://localhost:3500/api/diaries', body)
+    const res = await axios.post<JournalRequest>('http://localhost:3500/api/diaries', body) // localhost:3500 부분을 상수나 환경변수로
     return res;
 };
 
--- aifront/src/Page/Journal/JournalCheck.tsx
+++ aifront/src/Page/Journal/JournalCheck.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import axios from "axios";
 
 const JournalCheck : React.FC = () => {
-    
-    const MyJournal = async () => {
+
+    const MyJournal = async () => { // 함수 명도 영어 규칙에 맞게 쓰길 권장합니다. 동사 + 명사. ex) fetchMyJournal, getMyJournal
     try {
         await axios.get('URL');
         console.log('res');
@@ -11,8 +11,9 @@ const JournalCheck : React.FC = () => {
         console.log(err);
     };
     }
-    
-    MyJournal();
+
+    MyJournal(); // 이렇게 컴포넌트 내에서 바로 함수 호출되면 컴포넌트라 렌더링 될 때마다 함수가 불필요하게 호출됩니다. useEffect(() => {}, [])
+    // useEffect 말고 react-query 사용도 고려해보시면 좋을 듯!
 
     return(
         <></>
