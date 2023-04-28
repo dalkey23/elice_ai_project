e18861872f9ae011550e50b1384c6bbb34505c7c
SeowonOh <keyblossom.sw@gmail.com>
2023-04-28 21:22:01 +0900

    fix: 커뮤니티 페이지 css 수정

--- aifront/src/Page/Community/CommunityList.tsx
+++ aifront/src/Page/Community/CommunityList.tsx
@@ -1,30 +1,117 @@
-import React, { useState } from "react";
+import React, { useState, ReactNode } from "react";
 import axios from "axios";
-import { CommunityListMain, ButtonDiv } from "./CommunityListSC";
+import * as SC from "./CommunityListSC";
+import { BoardModel } from "../../Types/Community.type";
 
-const CommunityList : React.FC = () => {
-    const [journal, setJournal] = useState('');
+const tableHeadData = [
+    { id: 0, head: "글머리" },
+    { id: 1, head: "제목" },
+    { id: 2, head: "날짜" },
+    { id: 3, head: "작성자" },
+    { id: 4, head: "조회수" },
+];
 
-    const handleSubmit = async (e : React.MouseEvent) => {
-        e.preventDefault();
+const data = [
+  {
+    "id": 1,
+    "category": "맛집",
+    "title": "제목",
+    "content": "내용",
+    "writer": "가나다",
+    "views": 30,
+    "createdAt": "2023-04-11T23:11:12:037"
+  },
+  {
+    "id": 2,
+    "category": "레시피",
+    "title": "제목",
+    "content": "내용",
+    "writer": "가나다",
+    "views": 30,
+    "createdAt": "2023-04-11T23:11:12:037"
+  },
+]
 
-        try {
-            await axios.post('URL', {journal});
-            alert('작성이 완료 되었습니다!');
-        } catch(err) {
-            console.log(err);
-            alert('다시 작성해 주세요.');
-        }
-
-    }
-    return(
-        <CommunityListMain>
-            <h1>커뮤니티</h1>
-            <ButtonDiv>
-                <button onClick = {handleSubmit}>글쓰기</button>
-            </ButtonDiv>
-        </CommunityListMain>
-    )
+interface TableProps {
+    children: ReactNode;
+    tableHeadData?: {
+        id: number;
+        head: string;
+    }[];
+    posts: BoardModel[];
 }
+
+const Table = ({ children, tableHeadData, posts }: TableProps) => {
+    return (
+        <SC.StyledTable>
+            {tableHeadData && (
+                <SC.TableHead>
+                    <tr>
+                        {tableHeadData.map((item, index) => (
+                            <th key={index}>{item.head}</th>
+                        ))}
+                    </tr>
+                </SC.TableHead>
+            )}
+            <SC.TableBody>
+                {children}
+                {posts && posts.length === 0 && (
+                    <tr>
+                        <td colSpan={99}>게시글이 없습니다.</td>
+                    </tr>
+                )}
+            </SC.TableBody>
+        </SC.StyledTable>
+    );
+};
+
+const CommunityList: React.FC = () => {
+   const [posts, setPosts] = useState<BoardModel[]>(data);
+
+
+    const handleSubmit = async (e: React.MouseEvent) => {
+        e.preventDefault();
+    };
+
+    return (
+        <SC.CommunityListMain>
+            <h1>커뮤니티</h1>
+            <SC.CommunityListTable>
+                <tr>
+                    <td>글머리</td>
+                    <td>제목</td>
+                    <td>날짜</td>
+                    <td>작성자</td>
+                    <td>조회수</td>
+                </tr>
+            </SC.CommunityListTable>
+            <Table tableHeadData={tableHeadData} posts={posts!}>
+                {posts &&
+                    posts!.map((item, index) => {
+                        return (
+                            <tr
+                                key={item.id}
+                                onClick={() => {
+                                    //router.push(`/found/${item.id}`);
+                                }}
+                            >
+                                <td>
+                                    <SC.CategoryBadge text={item.category} />
+                                </td>
+                                <td width="400">{item.title} (0)</td>
+                                <td>{item.createdAt}</td>
+                                <td>{item.writer}</td>
+                                <td>{item.views}</td>
+                            </tr>
+                        );
+                    })}
+            </Table>
+
+            <SC.ButtonDiv>
+                <button onClick={handleSubmit}>글쓰기</button>
+            </SC.ButtonDiv>
+        </SC.CommunityListMain>
+    );
+};
 
 export default CommunityList;
--- aifront/src/Page/Community/CommunityListSC.tsx
+++ aifront/src/Page/Community/CommunityListSC.tsx
@@ -1,29 +1,113 @@
 import styled from "styled-components";
 
-export const CommunityListMain = styled.div`
-    & h1 {
-        display : flex;
-        justify-Content : center;
-        margin : 1em;
-        color : #3D2C8D;
+const colors = {
+    gray1: "#212428",
+    gray1WithOpacity20: "#212428B3",
+    gray2: "#62676c",
+    gray3: "#8b8d90",
+    gray4: "#b1b3b6",
+    gray5: "#e2e2e2",
+    gray6: "#efefef",
+    gray7: "#f9f9f9",
+    white: "#ffffff",
+    primary1: "#fdf301",
+    primary1WithOpacity20: "#fdf30120",
+    primary2: "#ede300",
+    primary3: "#5b5800",
+    systemError: "#e45c49",
+    systemSuccess: "#74d900",
+};
+
+export const StyledTable = styled.table`
+    width: 100%;
+    font-size: 16px;
+    margin-bottom: 40px;
+`;
+
+export const TableHead = styled.thead`
+    background-color: ${colors.gray7};
+    border: 1px solid ${colors.gray2};
+    border-width: 1px 0px;
+
+    th {
+        padding: 24px 32px;
+        font-weight: 600;
+        color: ${colors.gray2};
     }
-`
+`;
 
-export const ButtonDiv = styled.div`
-    display : flex;
-    justify-Content : right;
-    padding : 1em;
+export const TableBody = styled.tbody`
+    tr:nth-of-type(1) {
+        border-top: 1px solid ${colors.gray5};
+    }
+    tr {
+        border-bottom: 1px solid ${colors.gray5};
+        font-weight: 400;
+        color: ${colors.gray3};
+        cursor: pointer;
 
-    & button {
-        margin : 2em;
-        padding : 1em;
-        width : 7%;
-        color : white;
-        background-color : #916BBF;
-        border : 1px solid white;
-        border-Radius : 10px;
-        : hover {
-            background-color : #C996CC;
+        td {
+            padding: 24px 32px;
+            text-align: center;
+            vertical-align: middle;
         }
     }
-`
+
+    tr:hover {
+        background-color: ${colors.gray7};
+        color: ${colors.gray1};
+    }
+`;
+
+interface CategoryBadgeProps {
+    text: string;
+}
+
+export const CategoryBadge = ({ text }: CategoryBadgeProps) => {
+    return <StyledBadge>{text}</StyledBadge>;
+};
+
+const StyledBadge = styled.div`
+    background-color: ${colors.primary1WithOpacity20};
+    border: 1px solid ${colors.primary1};
+    border-radius: 40px;
+    width: 88px;
+    padding: 11px;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 18px;
+    text-align: center;
+    color: ${colors.primary3};
+`;
+
+export const CommunityListMain = styled.div`
+    padding: 100px;
+
+    & h1 {
+        display: flex;
+        justify-content: center;
+        margin: 1em;
+        color: #3d2c8d;
+    }
+`;
+
+export const CommunityListTable = styled.div``;
+
+export const ButtonDiv = styled.div`
+    display: flex;
+    justify-content: right;
+    padding: 1em;
+
+    & button {
+        margin: 2em;
+        padding: 1em;
+        width: 7%;
+        color: white;
+        background-color: #916bbf;
+        border: 1px solid white;
+        border-radius: 10px;
+        :hover  {
+            background-color: #c996cc;
+        }
+    }
+`;
