d0cc74dfc1e8b4df462c57befae8c533a6f88da6
SeowonOh <keyblossom.sw@gmail.com>
2023-05-04 17:39:21 +0900

    fix: 게시글 작성 페이지 작성

--- aifront/src/Types/Community.type.ts
+++ aifront/src/Types/Community.type.ts
@@ -2,9 +2,15 @@ export type BoardModel = {
     id: number;
     category: string;
     title: string;
-    comments: number,
+    comments: number;
     content: string;
     writer: string;
     views: number;
     createdAt: string;
+}
+
+export type PostBoard = {
+    category: string;
+    title: string;
+    content: string;
 }
