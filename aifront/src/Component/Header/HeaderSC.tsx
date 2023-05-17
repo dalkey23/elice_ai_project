481a4227591268e775d3321a0e731626ed0a9a82
[AI6/2반] 박재희 <dalkey23@gmail.com>
2023-05-17 21:40:31 +0900

    fix: 회원가입, 로그인 css 수정

--- aifront/src/Component/Header/HeaderSC.tsx
+++ aifront/src/Component/Header/HeaderSC.tsx
@@ -1,15 +1,39 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
-    padding : 3em;
-    font-size : 20px;
-    background-Color : #FFFFFF;
-    color : white;
-    border-radius : 8px;
+    padding: 1em;
+    font-size: 20px;
+    background-color: #ffffff;
+    color: white;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 
     & a {
-        padding : 1em;
-        text-Decoration : none;
-        color : black;
+        padding: 1em;
+        text-decoration: none;
+        color: black;
+        vertical-align: middle;
     }
-`
+
+    & img {
+        padding: 0;
+        width: 7em;
+        margin: 0;
+    }
+    & div {
+        position: absolute;
+        right: 2em;
+    }
+
+    & button {
+        border : none;
+        background-color: #ffffff;
+        font-size: 20px;
+    }
+    .LogoutDiv {
+        display: flex;
+        align-items: center;
+    }
+`;
--- aifront/src/Page/User/LoginSC.tsx
+++ aifront/src/Page/User/LoginSC.tsx
@@ -1,62 +1,60 @@
 import styled from "styled-components";
 
 export const LoginContainer = styled.div`
-    margin : 50px;
-    display : flex;
-    flex-direction : column;
-`
-
+    margin: 50px;
+    display: flex;
+    flex-direction: column;
+`;
 
 export const InfoDiv = styled.div`
-    margin : 20px auto;
-    padding : 10px;
-`
+    margin: 20px auto;
+    padding: 10px;
+`;
 
 export const AddedDiv = styled.div`
-    margin : 20px auto;
-    padding : 10px;
+    margin: 20px auto;
+    padding: 10px;
 
     & button {
-        margin : 20px;
-        color : #C996CC;
-        font-weight : bold;
-        background : none;
-        border : none;
-
+        margin: 20px;
+        color: #c996cc;
+        font-weight: bold;
+        background: none;
+        border: none;
     }
-
-`
+`;
 
 export const LoginItem = styled.div`
-    display : block;
+    display: block;
 
     & input {
         margin: 10px;
         border: solid 1px #3d2c8d;
+        border-radius : 10px;
         width: 400px;
         height: 50px;
     }
     & input::placeholder {
-        font-weight : bold;
-        color : gray;
-        text-align : center;
+        font-weight: bold;
+        color: gray;
+        text-align: center;
     }
-`
+`;
 
 export const ButtonDiv = styled.div`
-    padding : auto;
-    display: flex; 
+    padding: auto;
+    display: flex;
     justify-content: center;
-
-    
-`
+`;
 
 export const ConfirmButton = styled.button`
-    width: 200px;
-    height: 40px;
-    background: #3D2C8D;
-    text-align: center;
-    color: #FFFFFF;
-    margin : 10px;
-
-`
+    margin : 2em;
+    padding : 1em;
+    width : 15%;
+    color : white;
+    background-color : #916BBF;
+    border : 1px solid white;
+    border-Radius : 10px;
+    : hover {
+        background-color : #C996CC;
+`;
